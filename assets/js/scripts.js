8ca1304bf80a7fa063f1f8f6e4cd2b611d624dcf
Fernando Taladriz <fernando.taladriz@gmail.com>
2022-04-15 19:48:37 -0400

    estructura html inicial

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+// JAVASCRIPT (para los tooltip de Bootstrap)
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
